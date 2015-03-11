848054451e1151d2039e6206077580d494ae0de8
JUI-EN <julian820104@gmail.com>
2015-03-11 11:04:36 +0800

    Name

--- main.c
+++ main.c
@@ -4,9 +4,11 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
+	char name[50]="Yu-Hsun Lee";
 	printf("hello world\n");
 	printf("%d\n", 2+3);  //%d print an integer
 	printf("%f\n", 1.2+334.654); // %f print a float number 
-	printf("%f\n",sin(2.0*M_PI/3.0));
+	printf("%.16f\n",sin(2.0*M_PI/3.0));
+	printf("%s\n",name);
 	return 0;
 }
